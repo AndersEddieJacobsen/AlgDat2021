fb114d855018ce230fb709d36320582e1fc6d752
anders <anderseddiejacobsen@gmail.com>
2021-08-25 09:55:55 +0200

    Ny

---
+++ src/oslomet/Main.java
@@ -0,0 +1,12 @@
+package oslomet;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world");
+        String hei = JOptionPane.showInputDialog("haallo?");
+    }
+}
